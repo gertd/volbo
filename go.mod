3ce7fcc7e5a99aec3174534b825908da146840b8
Gert Drapers <gert.drapers@live.com>
2022-04-09 23:57:55 -0700

    set readers

--- go.mod
+++ go.mod
@@ -9,5 +9,6 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/objx v0.3.0 // indirect
 	github.com/zalando/go-keyring v0.1.1
+	golang.org/x/crypto v0.0.0-20220408190544-5352b0902921
 	golang.org/x/sys v0.0.0-20220204135822-1c1b9b1eba6a // indirect
 )
--- pkg/cmd/set.go
+++ pkg/cmd/set.go
@@ -2,16 +2,20 @@ package cmd
 
 import (
 	"bytes"
+	"log"
 	"os"
 
 	"github.com/gertd/volbo/pkg/cc"
 	"github.com/gertd/volbo/pkg/keyring"
+	"github.com/pkg/errors"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 type SetCmd struct {
-	Key   string   `arg:"" name:"key" required:"" help:"secret name"`
-	Value string   `xor:"group" required:"" name:"value" help:"secret value"`
-	Input *os.File `xor:"group" required:"" name:"file" type:"existingfile" help:"secret value from file or stdin"`
+	Key    string `arg:"" name:"key" required:"" help:"secret name"`
+	Prompt bool   `xor:"group" required:"" name:"prompt" help:"secret value read from secure prompt"`
+	Stdin  bool   `xor:"group" required:"" name:"stdin" help:"secret value read from stdin"`
+	File   string `xor:"group" required:"" name:"file" type:"existingfile" help:"secret value read from file"`
 }
 
 func (cmd *SetCmd) Run(c *cc.CommonCtx) error {
@@ -19,14 +23,27 @@ func (cmd *SetCmd) Run(c *cc.CommonCtx) error {
 		token string
 	)
 
-	if cmd.Input != nil {
+	switch {
+	case cmd.Stdin:
 		buf := new(bytes.Buffer)
-		if _, err := buf.ReadFrom(cmd.Input); err != nil {
-			return err
+		if _, err := buf.ReadFrom(os.Stdin); err != nil {
+			return errors.Wrapf(err, "read from stdin")
 		}
 		token = buf.String()
-	} else if cmd.Value != "" {
-		token = cmd.Value
+
+	case cmd.File != "":
+		buf, err := os.ReadFile(cmd.File)
+		if err != nil {
+			return errors.Wrapf(err, "read file [%s]", cmd.File)
+		}
+		token = string(buf)
+
+	case cmd.Prompt:
+		buf, err := terminal.ReadPassword(0)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		token = string(buf)
 	}
 
 	kr, err := keyring.New(cmd.Key)
